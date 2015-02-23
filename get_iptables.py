9d1c313d9d2f1cfb578fb5cca339eb807188c984
huh? <david.sackmary@utexas.edu>
2015-02-22 18:14:39 -0800

    final version

--- get_iptables.py
+++ get_iptables.py
@@ -1,10 +1,20 @@
+### Get_iptables.py 
+### INPUT: 'server_list.txt' from the same directory,
+###         unless a different file is supplied as an arg.  
+###         Each line in server_list.txt should contain 3 values:  
+###         ip_address user pwd
+### OUTPUT: A set of files in the same directory.
+###         Each is named by the ip_address of its' corresponding line.
+###       
+### Note: More error checking should be done for bad/down servers.
+
 import paramiko, argparse, csv
 
-def get_Iptables(ip, id, password):
-    trans = paramiko.Transport((ip, 22))
+def get_Iptables(host, id, password):
+    trans = paramiko.Transport((host, 22))
     trans.connect(username=id, password=password)
     session = trans.open_channel("session")
-    session.exec_command('sudo iptables -L')
+    session.exec_command('sudo iptables -L -v -n')
     session.recv_exit_status()
     while session.recv_ready():
         return session.recv(1024)
@@ -17,14 +27,12 @@ args = parser.parse_args()
 args.debug = False
 server_list = list(csv.reader(open(args.input, 'rb'), delimiter=' ', skipinitialspace=True))
 for server in server_list:
-    ip = server[0]
+    host = server[0]
     id = server[1]
     password = server[2]
-    if args.debug:
-        print ('Server IP: %s\nLogin ID: %s\nLogin Password: %s\n' % (ip, id, password))
 
-    iptables = get_Iptables(ip, id, password)
-    outfile = ip + "_iptables"
+    iptables = get_Iptables(host, id, password)
+    outfile = host + "_iptables"
     file = open(outfile,"w")
     file.write(iptables)
     file.close()
